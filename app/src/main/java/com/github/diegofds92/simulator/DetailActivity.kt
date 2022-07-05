9c2836641598abb1c4ce2c60b96c7aa3f076b8ff
diegofds92 <diego_fsilva92@hotmail.com>
2022-07-05 20:25:48 -0300

    Atualização da main activity

--- app/src/main/java/com/github/diegofds92/simulator/DetailActivity.kt
+++ app/src/main/java/com/github/diegofds92/simulator/DetailActivity.kt
@@ -2,10 +2,20 @@ package com.github.diegofds92.simulator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.diegofds92.simulator.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
